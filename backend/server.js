b4b13f2c63d491eb91b53cff032cd6ed0c0ed534
Coder2003Anujyoti <anujyotide@gmail.com>
2025-01-09 22:33:07 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,29 @@ const express = require('express');
 const fs=require('fs');
 const app = express();
 const PORT = 8001;
-const reaction=require("./text.json");
+const data=require("./text.json");
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const mongoose = require('mongoose');
+const URL = 'mongodb+srv://anujyotide:cr7FUgn1CxHAHUxo@benapi.x6nes.mongodb.net/About?retryWrites=true&w=majority';
+const connectionParams={ useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(URL,connectionParams)
+    .then(() => { console.log("Connected to the database");
+    addDataToMongodb();
+    }).catch((err)=>{console.log(err)})
+    async function addDataToMongodb() {
+    await GFGCollection
+        .deleteMany();
+    await GFGCollection
+        .insertMany(data);
+    console.log("Data added to MongoDB");
+}
+const gfgSchema = new mongoose
+    .Schema({
+        text: { type: String, required: true },
+    });
+    const GFGCollection = mongoose
+    .model("collections", gfgSchema);
 app.use(cors({
   origin:"*"
 }));
@@ -19,45 +39,26 @@ app.get('/',
             Hello Anujyoti!</h1>`
         );
     });
-app.get('/app/reaction', (req, res) => {
-  const limit = parseInt(req.query.limit)||reaction.length;  // Default to 10 if not provided
-  const offset = parseInt(req.query.offset)||0;  // Default to 0 if not provided
-if(isNaN(limit) && limit<=0){
-  return res.status(400).json({error:"Limit must be a positive number."})
-}
-if(isNaN(offset) && offset<0){
-  return res.status(400).json({error:"Offset must be a non-negative number."})
-}
-
-// Send the posts array as a JSON response
-return res.json(reaction.slice(offset,offset+limit));
-});
-app.get('/app/reaction/:id',(req,res)=>{
-  const id=Number(req.params.id);
-  const alien=reaction.find(i=> i.index===id)
-  return res.json(alien);
+app.get('/app/reaction',async(req,res)=>{
+  try {
+        const data = await GFGCollection.find();
+       return res.json(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
 })
-app.get('/reaction',(req,res)=>{
-  const html=`
-  <ul>
-  ${reaction.map(i=>`<li>${i.text}</li>`).join("")}
-  </ul>
-  `
-  res.send(html);
-})
-app.post('/app/reaction',(req,res)=>{
-  const data={
-    id:reaction.length+1,
-    text:req.body.text,
-  }
-  reaction.push(data);
-  fs.writeFile("./text.json",JSON.stringify(reaction), (err) => {
-  if (err) {
-    console.error('Error appending to file', err);
+app.post('/app/reaction', async (req, res) => {
+  try {
+    const newSample = await GFGCollection({
+      text:req.body.text,
+    });
+    await newSample.save({ w: "majority" });
+    return res.status(201).json(newSample);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
- return res.json({message:"Data successfully send"});
-})
 
 app.listen(PORT,
     () => {
