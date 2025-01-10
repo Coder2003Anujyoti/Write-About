f97ee3379a1ce06da92a344354bf85053b367171
Coder2003Anujyoti <anujyotide@gmail.com>
2025-01-10 21:53:09 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,8 @@ app.post('/app/reaction', async (req, res) => {
       text:req.body.text,
     });
     const saved=await newSample.save({w:"majority"});
+    data.push({text:req.body.text});
+    fs.writeFile("./text.json",JSON.stringify(data));
     return res.status(201).json(saved);
   } catch (err) {
     res.status(500).json({ error: err.message });
