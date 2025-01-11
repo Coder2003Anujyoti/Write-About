1ee44b69f8861e1c2d6de9c64e87522ffa2eb9fc
Coder2003Anujyoti <anujyotide@gmail.com>
2025-01-11 08:21:02 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -6,26 +6,6 @@ const PORT = 8001;
 const data=require("./text.json");
 const cors=require('cors');
 const bodyParser=require('body-parser');
-const mongoose = require('mongoose');
-const URL = 'mongodb+srv://anujyotide:cr7FUgn1CxHAHUxo@benapi.x6nes.mongodb.net/About?retryWrites=true&w=majority';
-const connectionParams={ useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(URL,connectionParams)
-    .then(() => { console.log("Connected to the database");
-    addDataToMongodb()
-    }).catch((err)=>{console.log(err)})
-    async function addDataToMongodb() {
-    await GFGCollection
-        .deleteMany();
-    await GFGCollection
-        .insertMany(data);
-    console.log("Data added to MongoDB");
-}
-const gfgSchema = new mongoose
-    .Schema({
-        text: { type: String, required: true },
-    });
-    const GFGCollection = mongoose
-    .model("collections", gfgSchema);
 app.use(cors({
   origin:"*"
 }));
@@ -39,27 +19,19 @@ app.get('/',
             Hello Anujyoti!</h1>`
         );
     });
-app.get('/app/reaction',async(req,res)=>{
-  try {
-        const data = await GFGCollection.find();
-       return res.json(data);
-    } catch (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-    }
+app.get('/app/reaction',(req,res)=>{
+       return res.status(201).json(data);
 })
-app.post('/app/reaction', async (req, res) => {
-  try {
-    const newSample = await GFGCollection({
-      text:req.body.text,
-    });
-    const saved=await newSample.save({w:"majority"});
-    data.push({text:req.body.text});
-    fs.writeFile("./text.json",JSON.stringify(data));
-    return res.status(201).json(saved);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+app.post('/app/reaction',(req, res) => {
+  const item={
+    text:req.body.text
   }
+    data.push(item);
+    fs.writeFile("./text.json",JSON.stringify(data),(err,data)=>{
+      if(err)
+      console.log(err)
+    });
+    return res.json(data);
 });
 
 app.listen(PORT,
